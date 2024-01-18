1437ac7fc0cab0a80303dfc58322eb772db321ab
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2024-01-18 12:15:21 +0500

    code

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,33 +1,23 @@
-import './globals.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
-import { Inter } from 'next/font/google'
+import './globals.css';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'WE App',
   description: 'Created By Abdul Ahad',
-}
+};
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
       </head>
-      <body className={inter.className}>
-          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+      <body>
+        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
       </body>
     </html>
-  )
+  );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -3,16 +3,16 @@
 import React from 'react'
 import { md2Theme, md3Theme } from './Mui/client.mjs';
 import { ThemeProvider } from '@emotion/react';
-import { Button } from '@mui/material-next';
 
-import MD2Button from '@mui/material/Button';
-import MD3Button from '@mui/material-next/Button';
 
 const page = () => {
   return (
-    <ThemeProvider theme={md3Theme}>
-        <Button variant="filled">Hello World</Button>
-  </ThemeProvider>
+    <>
+      <>
+        <ThemeProvider theme={md2Theme}>
+        </ThemeProvider>
+      </>
+    </>
   )
 }
 
